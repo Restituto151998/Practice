5ffa0864f6f10099eab53a6a32444318067c9d25
1styrGroupB <1styrGroupB@pn.local>
2020-12-26 14:54:14 +0800

    updated!

---
+++ public/practice.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>practice</title>
+</head>
+<body>
+  
+    <?php
+    $number = 25;
+    $Number = 0;
+    echo '<h1 style = "color:#AFA;">  Resti </h1>';
+    echo ($number)
+
+
+
+
+    ?>
+</body>
+</html>
